acaccee3ce66c9169cb81f37d2cc5f26d81fd66d
varunpintu100 <49563340+varunpintu100@users.noreply.github.com>
2022-04-02 18:55:50 +0530

    Removing the Django Project

--- DjangoProject/plotlist/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template.context_processors import request
-# Create your views here.
-def index(request):
-    return render(request,'index.html',{'name':'Home'})
